e491b156a3351a12f207a1ed7d56623e6e9abb54
kyllo34 <kyllo34@gmail.com>
2019-11-22 09:01:32 -0800

    added a for loop

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,37 @@
 'use strict';
-var x = confirm('Press OK if you are not a robot.');
-var responseTime = prompt('What time is it? (in hours <= 24)');
-var greeting;
 
-if (responseTime > 18) {
-  greeting = 'Good evening';
-} else if (responseTime > 12) {
-  greeting = 'Good afternoon';
-} else if (responseTime > 0) {
-  greeting = 'Good morning';
-} else {
-  greeting = 'Welcome';
+function greetUser() {
+  var humanoid = prompt('Are you human?');
+  var responseTime = prompt('What time is it?');
+  var greeting;
+
+  while(responseTime < 0 || responseTime > 24 || isNaN(responseTime) === true){
+    responseTime = prompt('Please enter a number between 0 and 24.');
+  }
+
+  if (responseTime > 18) {
+    greeting = 'Good evening';
+  } else if (responseTime > 12) {
+    greeting = 'Good afternoon';
+  } else if (responseTime > 0) {
+    greeting = 'Good morning';
+  } else {
+    greeting = 'Welcome';
+  }
+
+
+  if (humanoid === 'yes') {
+    var responseName = prompt('What is your name?');
+    document.write('<h3>' + greeting + ', ' + responseName + '!' + '</h3>');
+  } else {
+
+    document.write('<h3>' + greeting + '! ' + 'You are not allowed in here. Self-destruct initiated!' + '</h3>');
+  }
 }
-if (x === true) {
-  var responseName = prompt('What is your name?');
-  document.write('<h3>' + greeting + ', ' + responseName + '!' + '</h3>');
-} else {
-  document.write('<h3>' + greeting + '! ' + 'You are not allowed in here. Self-destruct initiated!' + '</h3>');
+function accountInfo() {
+  var accountNum = prompt('How many accounts do you want?');
+  for (var i = 1; i <= accountNum; i++) {
+    var accountAmt = prompt('How much money would you like in account' + i + '?');
+    document.write( i + '. $' + accountAmt + ' ');
+  }
 }
